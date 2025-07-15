5c11c9ac42a3caf087256074882bce72269a47fd
karpritam <karpritam194@gmail.com>
2025-07-15 17:17:06 +0530

    About section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,16 +39,16 @@ const Navbar = () => {
 			<div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
 				<img className="cursor-pointer" src={assets} alt="icon" />
 				<ul className="hidden md:flex gap-7 text-white">
-					<a href="#header" className="cursor-pointer hover:text-gray-400">
+					<a href="#Header" className="cursor-pointer hover:text-gray-400">
 						Home
 					</a>
-					<a href="#" className="cursor-pointer hover:text-gray-400">
+					<a href="#About" className="cursor-pointer hover:text-gray-400">
 						About
 					</a>
-					<a href="#" className="cursor-pointer hover:text-gray-400">
+					<a href="#Projects" className="cursor-pointer hover:text-gray-400">
 						Projects
 					</a>
-					<a href="#" className="cursor-pointer hover:text-gray-400">
+					<a href="#Testimonials" className="cursor-pointer hover:text-gray-400">
 						Testimonials
 					</a>
 				</ul>
