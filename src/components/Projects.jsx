5ca3a642456850bd84c31493c468785d99d8b4ce
karpritam <karpritam194@gmail.com>
2025-07-16 00:23:25 +0530

    projects section half

---
+++ src/components/Projects.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import left_arrow from '../assets/left_arrow.svg'
+import right_arrow from '../assets/right_arrow.svg'
+import {projectsData} from '../assets/assets'
+
+const Projects = () => {
+  return (
+    <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='Projects'>
+      <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
+      <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Building Legacies-Explore Our Portfolio</p>
+
+      {/* -----slider button---- */}
+      <div className='flex justify-end items-center mb-8'>
+        <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Previous Project'>
+            <img src={left_arrow} alt='previous' />
+        </button>
+        <button className='p-3 bg-gray-200 rounded mr-2' aria-label='Next Project'>
+            <img src={right_arrow} alt='Next' />
+        </button>
+      </div>
+
+      {/* -----slider container----- */}
+      <div>
+        <div>
+            {projectsData.map((project, index) => (
+        <div key={index}>
+          <h2>{project.title}</h2>
+          <p>{project.price}</p>
+          <p>{project.location}</p>
+          <img src={project.image} alt={project.title} />
+        </div>
+      ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
